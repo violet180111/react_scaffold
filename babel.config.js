80aeecd4fca6d24413dd1d595404ed9ecbb5a288
Qizheng 050711 <76-q050711@users.noreply.gitlab.topviewclub.cn>
2022-10-20 21:25:49 +0800

    docs: 补加注释

--- babel.config.js
+++ babel.config.js
@@ -4,12 +4,40 @@ const browserslist = {
 };
 const ENV = process.env.NODE_ENV;
 
+/**
+ * 更多内容 @see: https://webpack.docschina.org/
+ * @param presets - babel插件集合的预设,包含某一部分的插件plugin
+ * @see https://github.com/babel/babel
+ * @param presets[0] - @babel/preset-env - babel插件集合
+ * {
+ *    targets: 描述项目支持的浏览器环境
+ *    useBuiltIns: 根据配置的浏览器兼容，以及你代码中用到的 API 来进行 polyfill，实现了按需添加
+ *    corejs: corejs版本
+ * }
+ * @param presets[1] - @babel/preset-react - 所有 react 插件的 babel 预设
+ * {
+ *    runtime: 不再需要在react文件顶层引入React
+ *    importSource: 写上这句配置之后使用emotion就可以不用在文件顶部写  @jsxImportSource @emotion/react
+ * }
+ * @param presets[2] - @babel/preset-typescript - typeScript 的 babel 预设
+ * @param plugins - 用于转换代码
+ * @param plugins[0] - babel-plugin-import- 为组件库实现单组件按需加载并且自动引入其样式
+ * {
+ *    libraryName: 包名
+ *    libraryDirectory: 目录
+ *    libraryName: 是否引入 style
+ * }
+ *
+ *@param plugins[1] - @babel/plugin-transform-runtime - 自动移除语法转换后内联的辅助函数（inline Babel helpers），使用@babel/runtime/helpers里的辅助函数来替代
+ *{
+ *    corejs: 设置是否做API转换以避免污染全局环境
+ *}
+ */
 module.exports = {
   presets: [
     [
       '@babel/preset-env',
       {
-        // 项目所支持的浏览器的配置
         targets: browserslist[ENV],
         useBuiltIns: 'usage',
         corejs: '3',
@@ -19,7 +47,6 @@ module.exports = {
       '@babel/preset-react',
       {
         runtime: 'automatic',
-        // 写上这句配置之后使用emotion就可以不用在文件顶部写/* @jsxImportSource @emotion/react */
         importSource: '@emotion/react',
       },
     ],
@@ -45,8 +72,6 @@ module.exports = {
       '@babel/plugin-transform-runtime',
       {
         corejs: false,
-        helpers: true,
-        regenerator: true,
       },
     ],
     ENV === 'development' && 'react-refresh/babel',
